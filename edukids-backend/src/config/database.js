916a40e9889d946844f7e15679ddb82bf6143c69
pablo.m.santos <pablo.m.santos@accenture.com>
2019-05-28 20:37:43 -0300

    removido useMongoClient

--- edukids-backend/src/config/database.js
+++ edukids-backend/src/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-module.exports = mongoose.connect('mongodb://localhost/edukids', { useMongoClient: true })
+module.exports = mongoose.connect('mongodb://localhost/edukids')
 
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
 mongoose.Error.messages.Number.min = 
